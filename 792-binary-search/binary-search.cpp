2095b5034f2cd46f3bd361ae7755223941ce075e
Tanisha Sisodiya <24cs3057@rgipt.ac.in>
2025-06-20 22:14:05 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (45.40%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -3,15 +3,22 @@ public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
         int left = 0;
-        int right = nums.size();
-        while(left < right){
-            if(nums[left] == target){
-                return left;
+        int right = nums.size() -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid]== target){
+                return mid;
             }
-            left++;
+            if(nums[mid]< target){
+                left = mid + 1;
+            }
+            if(nums[mid]> target){
+                right = mid - 1;
+            }
         }
         return -1;
 
         }
+        
 
 };
