73d46a55b6fe88d947c0d74d022335a303e1e71a
Tanisha Sisodiya <24cs3057@rgipt.ac.in>
2025-10-07 00:23:20 +0530

    Time: 51 ms (55.60%) | Memory: 90.9 MB (31.22%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int>mp;
-        for(int i = 0; i<nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        for(int i = 0; i<nums.size(); i++){
-            if(mp[nums[i]]>= 2) return true;
-        }
-        return false;
+        unordered_set<int> seen;
+        for (int num : nums) {
+        if (seen.count(num))  return true;
+        seen.insert(num);
+    }
+    return false;
     }
 };
