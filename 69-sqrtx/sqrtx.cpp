6325e2ec2eebf7ebf69297706cb725c98cf6b38f
Tanisha Sisodiya <tanishasisodiya8@gmail.com>
2026-01-16 01:02:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (13.75%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,7 +1,32 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int ans  = 1LL*pow(x,0.5);
-        return ans;
+        if(x==0 || x==1)
+        return x;
+       
+       
+       
+        else{
+
+        
+
+        int s=0;
+        int e=x;
+        long long int  mid = s+ (e-s)/2;
+
+        
+        while(s<e){
+            
+            if(mid*mid <= x){
+                 s= mid+1;
+            }
+            else
+                e = mid;
+               
+            mid= s+(e-s)/2;
+        }
+        return s-1;
+        }
+        
     }
 };
