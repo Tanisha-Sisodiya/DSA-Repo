6f8598987f7a8e0e52b284ad08c70002d0238cae
Tanisha Sisodiya <tanishasisodiya8@gmail.com>
2026-01-30 20:44:23 +0530

    Time: 0 ms (100.00%) | Memory: 193.5 MB (54.46%) - LeetSync

---
+++ 4199-minimum-prefix-removal-to-make-array-strictly-increasing/minimum-prefix-removal-to-make-array-strictly-increasing.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumPrefixLength(vector<int>& nums) {
+    int n = nums.size();
+    int count = 0;
+    for(int i = n-1; i>0; i--){
+        if(nums[i] <= nums[i-1]){
+            count = i;
+            break;
+        }
+    }
+    return count;
+    }
+};
