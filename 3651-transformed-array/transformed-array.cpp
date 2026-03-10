8b9e759baae145730d3b5625690d9ddee90f065a
Tanisha Sisodiya <tanishasisodiya8@gmail.com>
2026-03-10 23:46:01 +0530

    Time: 4 ms (80.80%) | Memory: 25.7 MB (72.13%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+
+        for(int i = 0; i < n; i++){
+            int newIndex = ((i + nums[i]) % n + n) % n;
+            ans[i] = nums[newIndex];
+        }
+
+        return ans;
+    }
+};
