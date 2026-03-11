950d52dfad8a844aa8d89d038039e09f84734532
Tanisha Sisodiya <tanishasisodiya8@gmail.com>
2026-03-11 18:51:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (97.03%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq; //max heap by default
+        for(int i = 0; i< stones.size(); i++){
+            pq.push(stones[i]);
+        }
+        while(pq.size()>1){
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+            pq.push(y-x);
+        }
+        int ans = pq.top();
+        return ans;
+    }
+};
